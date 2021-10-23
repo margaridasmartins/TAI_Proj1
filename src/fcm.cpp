039136ebf03422da5698a9ebd8e99c3b597b009c
leand12 <leandrosilva12@ua.pt>
2021-10-23 09:19:00 +0100

    fix print table

--- src/fcm.cpp
+++ src/fcm.cpp
@@ -1,34 +1,39 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 #include <algorithm>
-#include <chrono>
 #include <iostream>
 #include <map>
 #include <set>
+#include <string>
+
+#include "utils.hpp"
 
 using namespace std;
 
 struct state {
-  map<char, int> occorrencies; // no need for ::iterator here
+  map<char, int> occorrencies;
   int sum;
 };
 
 map<string, state> table;
 set<char> symbols;
-uint k, a;
+uint k, a, threshold = 5;
 char line[100];
 
 void print_table() {
   printf("%6s ", "");
   for (auto s : symbols) {
-    printf("%4c ", s);
+    string c;
+    c += s;
+    printf("%4s ", c != "\n" ? c.c_str() : "\\n");
   }
   printf("\n");
 
   for (auto t : table) {
-    printf("%6s ", t.first.c_str());
+    string c = replace_all(t.first, "\n", "\\n");
+    printf("%6s ", c.c_str());
     for (auto s : symbols) {
       printf("%4d ", t.second.occorrencies[s]);
     }
@@ -36,23 +41,23 @@ void print_table() {
   }
 }
 
-void init_table(string context = "") {
+void init_table_arr(string context = "") {
   if (context.size() == k) {
     table[context] = {{}, 0};
     return;
   }
   for (auto s : symbols) {
-    init_table(context + s);
+    init_table_arr(context + s);
   }
 }
 
 void alphabet(FILE *fptr) {
   // estrutura com todos os simbolos
   while (fgets(line, 100, fptr) != NULL) {
-    if (symbols.size()==49){    //ELIMINAR NA ENTREGA!!
+    if (symbols.size() == 49) {  // ELIMINAR NA ENTREGA!!
       break;
     }
-    for (auto c:line){
+    for (auto c : line) {
       symbols.insert(c);
     }
   }
@@ -73,23 +78,27 @@ int main(int argc, char *argv[]) {
 
   if ((fptr = fopen(filename, "r")) == NULL) {
     printf("ERR: File not found\n");
-    // Program exits if the file pointer returns NULL.
     exit(1);
   }
 
-  alphabet(fptr); //check the characters in the file
-  rewind(fptr); //move the file position pointer back to the start of the file
-  
-  double tablesize=(pow(symbols.size(),k+1))/1024/1024;
-  cout << "Size of table: " << tablesize << "MB\n";  
+  alphabet(fptr);  // check the characters in the file
+  rewind(fptr);  // move the file position pointer back to the start of the file
 
-  init_table();
+  double tablesize = (pow(symbols.size(), k + 1)) / 1024 / 1024;
+
+  if (tablesize > threshold) {
+    // TODO: init_table_map();
+  } else {
+    init_table_arr();
+  }
   print_table();
 
+  printf("Size of table: %f MB\n", tablesize);
+
   while (fgets(line, 100, fptr) != NULL) {
-    //printf("The line is: %s\n", line);
+    // printf("The line is: %s\n", line);
   }
-  
+
   fclose(fptr);
 
   return 0;
